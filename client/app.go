310ea777e140ca5c6717098cd68809abc05df02b
PhilipGough <philip.p.gough@gmail.com>
2018-11-22 12:40:53 +0000

    Refactor CreateApp API to use modified response handler with test

--- client/app.go
+++ client/app.go
@@ -1,8 +1,8 @@
 package client
 
 import (
-	"encoding/xml"
 	"fmt"
+	"net/http"
 	"net/url"
 	"strings"
 )
@@ -11,7 +11,7 @@ const (
 	appCreate = "/admin/api/accounts/%s/applications.xml"
 )
 
-// CreateAp - Create an application.
+// CreateApp - Create an application.
 // The application object can be extended with Fields Definitions in the Admin Portal where you can add/remove fields
 func (c *ThreeScaleClient) CreateApp(accessToken string, accountId string, planId string, name string, description string) (Application, error) {
 	var apiResp Application
@@ -32,12 +32,10 @@ func (c *ThreeScaleClient) CreateApp(accessToken string, accountId string, planI
 
 	resp, err := c.httpClient.Do(req)
 	if err != nil {
-		return apiResp, genRespErr("create application", err.Error())
+		return apiResp, err
 	}
 	defer resp.Body.Close()
 
-	if err := xml.NewDecoder(resp.Body).Decode(&apiResp); err != nil {
-		return apiResp, genRespErr("create application", err.Error())
-	}
-	return apiResp, nil
+	err = handleXMLResp(resp, http.StatusCreated, &apiResp)
+	return apiResp, err
 }
