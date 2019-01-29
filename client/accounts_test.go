91277c6646daca7eef41ebb5d3e2481af4817d95
Eguzki Astiz Lezaun <eastizle@redhat.com>
2019-01-29 14:44:13 +0100

    client/accounts_test: refactor error tests using tabledriven pattern

--- client/accounts_test.go
+++ client/accounts_test.go
@@ -45,78 +45,54 @@ func TestListAccountsOk(t *testing.T) {
 	}
 }
 
-func TestListAccountsBadSyntax(t *testing.T) {
+func TestListAccountsErrors(t *testing.T) {
 	access_token := "someAccessToken"
-	httpClient := NewTestClient(func(req *http.Request) *http.Response {
-		if req.Method != http.MethodGet {
-			t.Fatalf("wrong helper called for account list api")
-		}
-
-		q := req.URL.Query()
-		if q.Get("access_token") != access_token {
-			t.Fatalf("Expected access token not found")
-		}
-
-		bodyReader := bytes.NewReader(helperLoadBytes(t, "accounts_wrong_format_fixture.xml"))
-
-		return &http.Response{
-			StatusCode: http.StatusOK,
-			Body:       ioutil.NopCloser(bodyReader),
-			Header:     make(http.Header),
-		}
-	})
-
-	c := NewThreeScale(NewTestAdminPortal(t), httpClient)
-
-	_, err := c.ListAccounts(access_token)
-	if err == nil {
-		t.Fatalf("account list did not return error")
+	errorTests := []struct {
+		Name                string
+		ResponseBodyFixture string
+		ExpectedErrorMsg    string
+		ErrorMsg            string
+	}{
+		{"BadSyntaxTest", "accounts_wrong_format_fixture.xml",
+			"XML syntax error", "expected error type is XML syntax error"},
+		{"DecodingErrorTest", "accounts_json_format_fixture.json",
+			"decoding error", "expected error type is decoding error"},
 	}
 
-	apiError, ok := err.(ApiErr)
-	if !ok {
-		t.Fatalf("expected ApiErr error type")
-	}
+	for _, tt := range errorTests {
+		t.Run(tt.Name, func(subTest *testing.T) {
+			httpClient := NewTestClient(func(req *http.Request) *http.Response {
+				if req.Method != http.MethodGet {
+					subTest.Fatalf("wrong helper called for account list api")
+				}
 
-	if !strings.Contains(apiError.Error(), "XML syntax error") {
-		t.Fatalf("expected error type is XML syntax error")
-	}
-}
+				q := req.URL.Query()
+				if q.Get("access_token") != access_token {
+					subTest.Fatalf("Expected access token not found")
+				}
 
-func TestListAccountsDecodingError(t *testing.T) {
-	access_token := "someAccessToken"
-	httpClient := NewTestClient(func(req *http.Request) *http.Response {
-		if req.Method != http.MethodGet {
-			t.Fatalf("wrong helper called for account list api")
-		}
+				bodyReader := bytes.NewReader(helperLoadBytes(subTest, tt.ResponseBodyFixture))
 
-		q := req.URL.Query()
-		if q.Get("access_token") != access_token {
-			t.Fatalf("Expected access token not found")
-		}
+				return &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       ioutil.NopCloser(bodyReader),
+					Header:     make(http.Header),
+				}
+			})
+			c := NewThreeScale(NewTestAdminPortal(t), httpClient)
+			_, err := c.ListAccounts(access_token)
+			if err == nil {
+				t.Fatalf("account list did not return error")
+			}
 
-		bodyReader := bytes.NewReader(helperLoadBytes(t, "accounts_json_format_fixture.json"))
+			apiError, ok := err.(ApiErr)
+			if !ok {
+				t.Fatalf("expected ApiErr error type")
+			}
 
-		return &http.Response{
-			StatusCode: http.StatusOK,
-			Body:       ioutil.NopCloser(bodyReader),
-			Header:     make(http.Header),
-		}
-	})
-
-	c := NewThreeScale(NewTestAdminPortal(t), httpClient)
-
-	_, err := c.ListAccounts(access_token)
-	if err == nil {
-		t.Fatalf("account list did not return error")
-	}
-
-	apiError, ok := err.(ApiErr)
-	if !ok {
-		t.Fatalf("expected ApiErr error type")
-	}
-
-	if !strings.Contains(apiError.Error(), "decoding error") {
-		t.Fatalf("expected error type is decoding error")
+			if !strings.Contains(apiError.Error(), tt.ExpectedErrorMsg) {
+				t.Fatalf(tt.ErrorMsg)
+			}
+		})
 	}
 }
