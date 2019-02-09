96d337ee90f341e3f317b335c983854f20d707a3
Eguzki Astiz Lezaun <eastizle@redhat.com>
2019-02-09 15:32:17 +0100

    show tenant operation

--- client/tenants.go
+++ client/tenants.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
 	"strings"
@@ -8,6 +9,7 @@ import (
 
 const (
 	tenantCreate = "/master/api/providers.json"
+	tenantRead   = "/master/api/providers/%d.json"
 )
 
 // CreateTenant creates new tenant using 3scale API
@@ -32,5 +34,28 @@ func (c *ThreeScaleClient) CreateTenant(orgName, username, email, password strin
 
 	tenant := &Tenant{}
 	err = handleJsonResp(resp, http.StatusCreated, tenant)
+	return tenant, err
+}
+
+// ShowTenant - Returns tenant info for the specified ID
+func (c *ThreeScaleClient) ShowTenant(accessToken string, tenantID int64) (*Tenant, error) {
+	endpoint := fmt.Sprintf(tenantRead, tenantID)
+	req, err := c.buildGetReq(endpoint)
+	if err != nil {
+		return nil, httpReqError
+	}
+
+	values := url.Values{}
+	values.Add("access_token", accessToken)
+	req.URL.RawQuery = values.Encode()
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	tenant := &Tenant{}
+	err = handleJsonResp(resp, http.StatusOK, tenant)
 	return tenant, err
 }
--- client/tenants_test.go
+++ client/tenants_test.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strings"
@@ -98,6 +99,95 @@ func TestCreateTenantErrors(t *testing.T) {
 			})
 			c := NewThreeScale(NewTestAdminPortal(t),credential, httpClient)
 			_, err := c.CreateTenant(orgName, userName, email, password)
+			if err == nil {
+				subTest.Fatalf("create tenant did not return error")
+			}
+
+			apiError, ok := err.(ApiErr)
+			if !ok {
+				subTest.Fatalf("expected ApiErr error type")
+			}
+
+			if !strings.Contains(apiError.Error(), tt.ExpectedErrorMsg) {
+				subTest.Fatalf("Expected [%s]: got [%s] ", tt.ExpectedErrorMsg, apiError.Error())
+			}
+		})
+	}
+}
+
+func TestShowTenantOk(t *testing.T) {
+	accessToken := "someAccessToken"
+	tenantID := int64(42)
+	httpClient := NewTestClient(func(req *http.Request) *http.Response {
+		if req.Method != http.MethodGet {
+			t.Fatalf("wrong http method called for create tenant endpoint")
+		}
+
+		q := req.URL.Query()
+		if q.Get("access_token") != accessToken {
+			t.Fatalf("Expected access token not found")
+		}
+
+		p := req.URL.Path
+		if p != fmt.Sprintf(tenantRead, tenantID) {
+			t.Fatalf("Path: expected (%s) found (%s)", fmt.Sprintf(tenantRead, tenantID), p)
+		}
+
+		bodyReader := bytes.NewReader(helperLoadBytes(t, "show_tenant_response.json"))
+		return &http.Response{
+			StatusCode: http.StatusOK,
+			Body:       ioutil.NopCloser(bodyReader),
+			Header:     make(http.Header),
+		}
+	})
+
+	c := NewThreeScale(NewTestAdminPortal(t), httpClient)
+
+	tenant, err := c.ShowTenant(accessToken, tenantID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if tenant == nil {
+		t.Fatalf("tenant not parsed")
+	}
+
+	if tenant.Signup.Account.SupportEmail != "admin@corp24.com" {
+		t.Fatalf("Email: expected (%s) found (%s)", "admin@corp24.com", tenant.Signup.Account.SupportEmail)
+	}
+
+	if tenant.Signup.Account.OrgName != "Corp24" {
+		t.Fatalf("OrgName: expected (%s) found (%s)", "Corp24", tenant.Signup.Account.OrgName)
+	}
+}
+
+func TestShowTenantErrors(t *testing.T) {
+	accessToken := "someAccessToken"
+	tenantID := int64(42)
+	errorTests := []struct {
+		Name                string
+		ResponseBodyFixture string
+		ExpectedErrorMsg    string
+		HTTPStatusCode      int
+	}{
+		{"CorruptedJsonErrorTest", "accounts_wrong_format_fixture.json", "decoding error - unexpected EOF", 201},
+		{"WrongFormatErrorTest", "xml_format_fixture.xml", "decoding error - invalid character", 201},
+		{"UnexpectedHTTPStatusCode", "error_response_fixture.json", "Test Error", 400},
+	}
+
+	for _, tt := range errorTests {
+		t.Run(tt.Name, func(subTest *testing.T) {
+			httpClient := NewTestClient(func(req *http.Request) *http.Response {
+				bodyReader := bytes.NewReader(helperLoadBytes(subTest, tt.ResponseBodyFixture))
+
+				return &http.Response{
+					StatusCode: tt.HTTPStatusCode,
+					Body:       ioutil.NopCloser(bodyReader),
+					Header:     make(http.Header),
+				}
+			})
+			c := NewThreeScale(NewTestAdminPortal(t), httpClient)
+			_, err := c.ShowTenant(accessToken, tenantID)
 			if err == nil {
 				subTest.Fatalf("create tenant did not return error")
 			}
