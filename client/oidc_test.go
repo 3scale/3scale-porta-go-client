b618d63a9dd8c5b4b8cd848bb94169e5fd75b48f
Eguzki Astiz Lezaun <eastizle@redhat.com>
2020-11-19 17:23:11 +0100

    oidc configuration endpoints

---
+++ client/oidc_test.go
@@ -0,0 +1,112 @@
+package client
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"testing"
+)
+
+func TestOidcConfiguration(t *testing.T) {
+	var (
+		productID int64 = 97
+		endpoint        = fmt.Sprintf(oidcResourceEndpoint, productID)
+		oidcConf        = &OIDCConfiguration{
+			Element: OIDCConfigurationItem{
+				StandardFlowEnabled:       false,
+				ImplicitFlowEnabled:       true,
+				ServiceAccountsEnabled:    false,
+				DirectAccessGrantsEnabled: true,
+			},
+		}
+	)
+
+	httpClient := NewTestClient(func(req *http.Request) *http.Response {
+		if req.URL.Path != endpoint {
+			t.Fatalf("Path does not match. Expected [%s]; got [%s]", endpoint, req.URL.Path)
+		}
+
+		if req.Method != http.MethodGet {
+			t.Fatalf("Method does not match. Expected [%s]; got [%s]", http.MethodGet, req.Method)
+		}
+
+		responseBodyBytes, err := json.Marshal(oidcConf)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		return &http.Response{
+			StatusCode: http.StatusOK,
+			Body:       ioutil.NopCloser(bytes.NewBuffer(responseBodyBytes)),
+			Header:     make(http.Header),
+		}
+	})
+
+	credential := "someAccessToken"
+	c := NewThreeScale(NewTestAdminPortal(t), credential, httpClient)
+	obj, err := c.OidcConfiguration(productID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if obj == nil {
+		t.Fatal("response was nil")
+	}
+
+	if *obj != *oidcConf {
+		t.Fatalf("Expected %v; got %v", *oidcConf, *obj)
+	}
+}
+
+func TestUpdateOidcConfiguration(t *testing.T) {
+	var (
+		productID int64 = 98765
+		endpoint        = fmt.Sprintf(oidcResourceEndpoint, productID)
+		oidcConf        = &OIDCConfiguration{
+			Element: OIDCConfigurationItem{
+				StandardFlowEnabled:       false,
+				ImplicitFlowEnabled:       true,
+				ServiceAccountsEnabled:    false,
+				DirectAccessGrantsEnabled: true,
+			},
+		}
+	)
+
+	httpClient := NewTestClient(func(req *http.Request) *http.Response {
+		if req.URL.Path != endpoint {
+			t.Fatalf("Path does not match. Expected [%s]; got [%s]", endpoint, req.URL.Path)
+		}
+
+		if req.Method != http.MethodPatch {
+			t.Fatalf("Method does not match. Expected [%s]; got [%s]", http.MethodPatch, req.Method)
+		}
+
+		responseBodyBytes, err := json.Marshal(*oidcConf)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		return &http.Response{
+			StatusCode: http.StatusOK,
+			Body:       ioutil.NopCloser(bytes.NewBuffer(responseBodyBytes)),
+			Header:     make(http.Header),
+		}
+	})
+
+	credential := "someAccessToken"
+	c := NewThreeScale(NewTestAdminPortal(t), credential, httpClient)
+	obj, err := c.UpdateOidcConfiguration(productID, oidcConf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if obj == nil {
+		t.Fatal("resp returned nil")
+	}
+
+	if *obj != *oidcConf {
+		t.Fatalf("Expected %v; got %v", *oidcConf, *obj)
+	}
+}
